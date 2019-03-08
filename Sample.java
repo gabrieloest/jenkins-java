ddc2680aaf96cfa82349a13dcc84635f1d85b32b
Gabriel Oest <bieloest@gmail.com>
2019-03-08 09:30:33 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
 public class Sample {
-  public static void main(String[] args){
-    System.out.println("Hello world!");
+  public boolean isGreater(int num1, int num2){
+    return num1 > num2;
   }
 }
