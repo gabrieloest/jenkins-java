837533ce34e09b6e8b5a93f3aec4a32bea7ea206
Gabriel Oest <Gabriel_Oest@paramount.com>
2019-03-08 11:19:28 -0800

    Add test

--- src/test/java/uy/com/jenkins/SampleTest.java
+++ src/test/java/uy/com/jenkins/SampleTest.java
@@ -1,6 +1,7 @@
 package uy.com.jenkins;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -11,30 +12,15 @@ import org.junit.jupiter.api.Test;
 
 public class SampleTest {
 
-	@BeforeAll
-	public static void beforeClass() {
-		System.out.println("Before Class");
-	}
-
-	@BeforeEach
-	public void before() {
-		System.out.println("Before Test Case");
-	}
-
 	@Test
 	public void isGreaterTest() {
-		System.out.println("Test");
 		Sample tester = new Sample();
-		assertTrue(tester.isGreater(2, 3));
+		assertTrue(tester.isGreater(3, 2));
 	}
-
-	@AfterEach
-	public void after() {
-		System.out.println("After Test Case");
-	}
-
-	@AfterAll
-	public static void afterClass() {
-		System.out.println("After Class");
+	
+	@Test
+	public void isNotGreaterTest() {
+		Sample tester = new Sample();
+		assertFalse(tester.isGreater(2, 3));
 	}
 }
